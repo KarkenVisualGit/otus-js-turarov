7afc3c42f2d6d3d6fb6476a5b2ca1aa6665ae535
Karken Turarov <karken_turarov@mail.ru>
2023-07-17 22:57:05 +0600

    added arrow function

--- contexts.js
+++ contexts.js
@@ -2,7 +2,16 @@ const person = {
     name:'John Smith',
     sayHi() {
         console.log (`Hi ${this.name}`);
+    },
+    changeName (str) {
+        this.name = str;
+    },
+    sayName: () => {
+        console.log(this.name);
     }
 }
 
 person.sayHi();
+person.changeName('Sam');
+console.log(person.name);
+person.sayName();
