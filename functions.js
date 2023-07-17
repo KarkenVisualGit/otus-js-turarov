7722141afc622504fdd728ce8930739e3b3398a5
Karken Turarov <karken_turarov@mail.ru>
2023-07-17 21:42:02 +0600

    add сдщыгкуы

--- functions.js
+++ functions.js
@@ -1,7 +1,10 @@
 function outer() {
     let message = 'Hello';
+    let count = 0;
     return function() {
         console.log(message);
+        count++;
+        console.log(count);
     }
 }
 
