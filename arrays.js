107ad6c163751cf3f0e8f60a0647906f6a135dfe
Karken Turarov <karken_turarov@mail.ru>
2023-07-17 17:58:52 +0600

    added reduce function

--- arrays.js
+++ arrays.js
@@ -13,21 +13,23 @@ const newnums = nums.forEach(element =>
     }
 )
 console.log(sum);
-
 const array = nums.map(x => x * 2);
 console.log(array);
 
-const map = nums.map (x =>
-    {
-        let max = 0;
-        let min = 0;
-        x > x[i+1] ? (max = x) : (min = x[i+1]);
-    }
-)
+const max = nums.reduce ( (a,b) => a > b ? a : b)
+console.log(max);
 
-for (let i = 0; i < nums.length; i++) {
-    
-}
-
-nums.splice
-console.log(map);
+const min = nums.reduce ( (a,b) => a < b ? a : b)
+console.log(min);
+// let max = 0;
+// let min = 0;
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = i; j < nums.length; j++) {
+//         if (nums[i] > nums[j]) {
+//             max = nums[i]; min = nums[j];
+//         }
+//         else {max = nums[j]; min = nums[i];}
+//     }   
+// }
+// console.log("Max = " + max);
+// console.log("Min = " + min);
