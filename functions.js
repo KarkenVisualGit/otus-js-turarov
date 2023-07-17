d970d2eb1e429547f1b52e1e4c420b0f508934fd
Karken Turarov <karken_turarov@mail.ru>
2023-07-17 22:13:48 +0600

    added callback

--- functions.js
+++ functions.js
@@ -7,6 +7,57 @@ function outer() {
         console.log(count);
     }
 }
+const id = setTimeout(() => {
+    
+}, 1000);
+clearTimeout (id);
+function logger() {
+    console.log('now');
+}
+setTimeout(logger, 1000);
+function sum(a,b) {
+    return a+b;
+}
+setTimeout (sum, 1000, 5, 2);
+// setInterval(() => {
+//     console.log('now');
+// }, 1000);
+
+function counter (from,to) {
+    let number = from;
+    const id = setInterval(() => {
+        console.log(number);
+        if (number == to) {
+            clearInterval(id);
+        } else {
+            number++;
+        }
+    }, 1000);
+}
+counter(4, 8);
+const request = (cb) => {
+    console.log('request');
+    cb({text: 'Error'});
+
+    setTimeout(() => {
+        console.log('response');
+        const data = {text:'Hello'};
+        cb(null, data);
+    }, 2000);
+}
+
+// const handler = (data) => {
+//     console.log('handler', data);
+// }
+
+// request (handler);
+request ((err, data) => {
+    if (!err) {
+        console.log('Hello callback!', data);
+    } else {
+        console.error(err);
+    } 
+});
 
 const value = outer();
 
