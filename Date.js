47df2a22d4a052ae4a68f63fc5cbf882a79bc8db
Karken Turarov <karken_turarov@mail.ru>
2023-07-21 01:34:47 +0600

    older function

--- Date.js
+++ Date.js
@@ -7,9 +7,29 @@ const mins= date.getMinutes();
 const sec= date.getSeconds();
 
 const secPass = (hours*3600) + (mins*60) + sec;
+let minPass = mins*60;
 const secInDay = 24*60*60;
+const minInDay = 24*60;
 
 console.log(secInDay - secPass);
+console.log(minPass);
+
+const input = prompt('DD.MM.YYYY');
+const [days, month, year] = input.split('.');
+
+const inputDate = new Date(`${year}-${month}-${days}`);
+
+function getWeekDay(date) {
+    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+  
+    return days[date.getDay()];
+  }
+
+let diff = Date.now() - inputDate;
+diff = diff / 1000 / 60 / 60;
+console.log(diff.toFixed(0));
+
+console.log(getWeekDay(inputDate));
 
 function formatDate(date) {
     let day = makeTwoDigit(date.getDate());
@@ -28,6 +48,26 @@ function makeTwoDigit(value) {
     }
     return newValue;
 }
+let persondate1 = prompt('DD.MM.YYYY');
+let persondate2 = prompt('DD.MM.YYYY');
+const personDate1 = new Date(persondate1);
+const personDate2 = new Date(persondate2);
+
+const person1 = {
+    date: personDate1,
+    name: 'John'
+};
+const person2 = {
+    date: personDate2,
+    name: 'Sam'
+}
+let diffDate = personDate1 - personDate2;
+diffDate = diffDate / 1000 / 60 / 60;
+function older() {
+    return diffDate < 0 ? person1.name : person2.name;
+}
+
+console.log( older() + " is older " + diff.toFixed(0) );
 
 const date1 = new Date('2007-06-05T05:15:44');
 console.log(formatDate(date1));
