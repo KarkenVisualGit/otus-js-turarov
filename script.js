19a6d20992ce1722b6e056a5d3bbb4bb7d96653b
Karken Turarov <karken_turarov@mail.ru>
2023-07-20 21:28:07 +0600

    edited scripts eventlistener

--- script.js
+++ script.js
@@ -2,43 +2,27 @@
 // const messageEl = document.createElement('p');
 // const messagebox = document.querySelector('.testdiv');
 // const inputEl = document.querySelector('.textInput');
-
-Button.addEventListener('click', hidden);
-    function hidden() {
-    const text = getInputText();
-    const Button = document.querySelector('.btn');
-        if( text === '' ) {
-            Button.hidden = true;
-        }
-        Button.removeEventListener('click', hidden);
-        Button.addEventListener('click', showButton);
-    }
-
-    function showButton() {
-        const Button = document.querySelector('.btn');
-        Button.hidden = false;
-
-        Button.removeEventListener('click', showButton);
-        Button.addEventListener('click', hidden);
-    }
-
+const Button = document.querySelector('.btn');
+const paragraph = document.querySelectorAll('p');
 Button.addEventListener('click', buttonClick);
-    function buttonClick() {
-        const text = getInputText();
-        addText(text);
-        clearInput();
-    }
 
-
-Button.addEventListener('click', removeParagraph);
-    function removeParagraph() {
+function buttonClick() {
+    const text = getInputText();
     const paragraph = document.querySelectorAll('p');
     const paragraphElement = document.querySelector('p');
-        if (paragraph.length > 5) {
+        if( paragraph.length >= 5) {
             paragraphElement.remove;
+            clearInput();
+         }    
+         else if (text === '' ) {
+            Button.hidden = true;
+        } else {
+            addText(text); 
+            clearInput();
         }
-         else (Button.removeEventListener('click', removeParagraph))
-    }
+    // Button.removeEventListener('click', buttonClick);
+    // Button.addEventListener('click', clearInput);
+}
 
 function getInputText() {
     const input = document.querySelector('.textInput');
@@ -53,9 +37,43 @@ function addText(input) {
     messagebox.append(messageElement);
 }
 
-Button.addEventListener('click', clearInput);
 function clearInput() {
     const input = document.querySelector('.textInput');
     input.value = '';
-    Button.removeEventListener('click', clearInput);
+
+    const Button = document.querySelector('.btn');
+    // Button.removeEventListener('click', clearInput);
+    Button.addEventListener('click', buttonClick);
 }
+
+// function showButton() {
+//     const text = getInputText();
+//     const Button = document.querySelector('.btn');
+   
+//         Button.hidden = false;
+          
+    
+//     Button.removeEventListener('click', showButton);
+//     Button.addEventListener('click', removeParagraph);
+// }
+    // function hidden() {
+    // const text = getInputText();
+    // const Button = document.querySelector('.btn');
+        
+    //     Button.hidden = true;
+
+    //     // Button.removeEventListener('click', hidden);
+    //     Button.addEventListener('click', buttonClick);
+    // }
+
+    // function removeParagraph() {
+    // const paragraph = document.querySelectorAll('p');
+    // const paragraphElement = document.querySelector('p');
+    // if( paragraph.length > 5) {
+    //         paragraphElement.remove;
+    // }
+    //     const Button = document.querySelector('.btn');
+    //     // Button.removeEventListener('click', removeParagraph);
+    //     Button.addEventListener('click', buttonClick);
+    // }
+
