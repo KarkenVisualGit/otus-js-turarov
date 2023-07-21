2e88f2f893c63e76a689444ef4c7c1a731f91998
Karken Turarov <karken_turarov@mail.ru>
2023-07-21 12:16:24 +0600

    added days output to diffDate

--- Date.js
+++ Date.js
@@ -49,25 +49,30 @@ function makeTwoDigit(value) {
     return newValue;
 }
 let persondate1 = prompt('DD.MM.YYYY');
+const [dd, mm, yr] = persondate1.split('.');
+const personDate1 = new Date(`${yr}-${mm}-${dd}`);
 let persondate2 = prompt('DD.MM.YYYY');
-const personDate1 = new Date(persondate1);
-const personDate2 = new Date(persondate2);
+const [dd1, mm1, yr1] = persondate2.split('.');
+const personDate2 = new Date(`${yr1}-${mm1}-${dd1}`);
+// const personDate1 = new Date(persondate1);
+// const personDate2 = new Date(persondate2);
 
 const person1 = {
     date: personDate1,
-    name: 'John'
+    name: 'John first'
 };
 const person2 = {
     date: personDate2,
-    name: 'Sam'
+    name: 'Sam second'
 }
 let diffDate = personDate1 - personDate2;
-diffDate = diffDate / 1000 / 60 / 60;
+diffDate = diffDate / 1000 / 60 / 60 / 24;
 function older() {
     return diffDate < 0 ? person1.name : person2.name;
 }
-
-console.log( older() + " is older " + diff.toFixed(0) );
+console.log(personDate1);
+console.log(personDate2);
+console.log( older() + " is older " + diffDate.toFixed(0) );
 
 const date1 = new Date('2007-06-05T05:15:44');
 console.log(formatDate(date1));
