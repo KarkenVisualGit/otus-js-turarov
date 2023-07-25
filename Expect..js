d4ed2d13ffde9d80f40733c71d40a7aa4ff5e7d8
Karken Turarov <karken_turarov@mail.ru>
2023-07-25 23:49:16 +0600

    added complex.js

---
+++ Expect..js
@@ -0,0 +1,6 @@
+const numbersList = [1, 2, 3]; // подготовка
+const indexOf5 = numbersList.indexOf(5); // выполнение
+expect(indexOf5).toBe(-1); // проверкаs
+
+expect([1, 2, 3].indexOf(5)).toBe(-1);
+
---
+++ complex.js
@@ -0,0 +1,11 @@
+var m = [
+    [0,  1,  2,  3,  4],
+    [5,  6,  7,  8,  9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19]
+  ];
+  
+  var spiral = function (m) {
+    return m.toString().split(',').join(); // Put your code here
+  };
+  console.log(spiral(m));
