5ddc96093656b3e1a168ecc5dfe64035e31a8dbd
Karken Turarov <karken_turarov@mail.ru>
2023-08-02 14:28:24 +0600

    added starSign

---
+++ starSign.js
@@ -0,0 +1,11 @@
+function starSign(date) {
+    if(date.getDate() >= 21 && date.getmonth() >=0 
+       && date.getDate() <= 19 && date.getmonth() <= 1) {
+      return 'Aquarius ';
+    } else if(date.getDate() >= 21 && date.getmonth() >=0 
+       && date.getDate() <= 19 && date.getmonth() <= 1) {
+      
+    }
+    
+    return ; //code here
+  }
