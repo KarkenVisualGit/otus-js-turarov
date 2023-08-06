3d1074509c1b5d5b8b65b70e37575c2489be62f0
Karken Turarov <karken_turarov@mail.ru>
2023-08-06 17:08:11 +0600

    added factorial

---
+++ src/quickSort.js
@@ -0,0 +1,13 @@
+const array = [3,2,0,-4,8,18,7,20,9,66,11,155,-13,9,45,-57,17,32,29,57,21];
+let count = 0;
+
+function quickSort(array) {     //recursive search
+
+}
+
+
+
+
+
+console.log(bquickSort(array));
+console.log("count = " + count);
