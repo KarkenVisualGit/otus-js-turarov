5158ca65e57ce1b1371941ee65db7852cdc80a8d
Karken Turarov <karken_turarov@mail.ru>
2023-08-09 12:19:41 +0600

    add json response creatinh innerHtml

--- src/main.js
+++ src/main.js
@@ -12,21 +12,49 @@
 
 
 const apikey = 'a6a19c057af84c20ac380535230808';
+const header = document.querySelector('.header');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
-let city;
 
 form.onsubmit = function (e) {
     e.preventDefault();
-    city = input.value.trim();
+    let city = input.value.trim();
 
-    
-    const url = 'http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}';
 
-    fetch(url).then((response) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}`;
+
+    fetch(url)
+    .then((response) => {
         return response.json()
     })
-    .then((data) => {
-        console.log(data);
-    })
+        .then((data) => {
+
+            if (data.error) {
+                const prevcard = document.querySelector('.card');
+                if (prevcard) prevcard.remove();
+
+                const html = `<div class="card">${data.error.message}</div>`;
+
+                header.insertAdjacentHTML('afterend', html);
+
+            } else {
+                const prevcard = document.querySelector('.card');
+                if (prevcard) prevcard.remove();
+
+                const html = `<div class="card">
+        <h2 class="card-city">${data.location.name}<span>${data.location.country}</span></h2>
+
+        <div class="card-weather">
+            <div class="card-value">${data.current.temp_c}<sup>°C</sup></div>
+            <img class="card-img" src="./images/4.png" alt="weather">
+        </div>
+
+        <div class="card-desc">${data.current.condition.text}</div>
+
+         </div>`;
+
+                header.insertAdjacentHTML('afterend', html);
+            }
+
+        })
 } 
