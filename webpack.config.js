3dce5d3383aa334fdae4823175a32fe18e0efc48
Karken Turarov <karken_turarov@mail.ru>
2023-08-11 17:20:32 +0600

    add webpack dependences

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    target:'web',
+    devtool:'source-map',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[contenthash:8].js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', "css-loader"],
+                },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/,
+                type: 'asset/resource',
+                generator:  {
+                    filename: 'images/[name]-[contenthash][ext]',
+                }
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator:  {
+                    filename: 'fonts/[name]-[contenthash][ext]',
+                }
+            }
+        ]
+    },
+    devServer: {
+        compress: false,
+        open: true,
+        port: 3000,
+        hot: true,
+    },
+}
